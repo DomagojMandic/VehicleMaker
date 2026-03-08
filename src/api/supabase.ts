699e3e26dd51d3599735faf58c45ee61cebb4eec
= <=>
2026-03-08 16:32:59 +0100

    Restored the DB to a new Project using PostgreSQL

--- src/api/supabase.ts
+++ src/api/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://jyghtfnpotoepbhjttis.supabase.co';
+const SUPABASE_URL = 'https://afthuwngkptytvugwwoz.supabase.co';
 const SUPABASE_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5Z2h0Zm5wb3RvZXBiaGp0dGlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3MTkyNzAsImV4cCI6MjA3NDI5NTI3MH0.u2iv7FXgVFaOhUAIUyvPeDNBQPhg2AQbsmQ1VtYtSTg';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmdGh1d25na3B0eXR2dWd3d296Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5ODA0OTIsImV4cCI6MjA4ODU1NjQ5Mn0.zA3e9oXD2DQ48IeiN2u02Rf7FutsFrAMWQVDfPv0_5k';
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export default supabase;
